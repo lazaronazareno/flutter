1ebe991b11114bc0e890a29be9c9a14bbf7bbf9e
lazaronazareno <lazaronazareno@gmail.com>
2024-12-13 19:16:39 -0300

    feat: :sparkles: video player design
    homework

--- lib/video_player_design/video_player_design.dart
+++ lib/video_player_design/video_player_design.dart
@@ -7,11 +7,12 @@ class VideoPlayerDesign extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    double heightAppBar = size.height * 0.1;
+    double heightAppBar = size.height * 0.15;
     double heightAlbumPlayer = size.height * 0.7;
-    double heightContinueWatching = size.height * 0.2;
+    double heightContinueWatching = size.height * 0.15;
 
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       body: Column(
         children: [
           AppBar(
@@ -35,10 +36,84 @@ class AppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
+    return SizedBox(
+      width: double.infinity,
       height: height,
-      child: const Center(child: Text("AppBar")),
+      child: Padding(
+        padding: const EdgeInsets.only(
+          top: 24,
+          left: 16,
+          right: 16,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Row(
+              children: [
+                CircleAvatar(
+                  radius: 20,
+                  backgroundColor: Colors.grey,
+                  backgroundImage: AssetImage("assets/images/avatar.png"),
+                ),
+                SizedBox(width: 8),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "HappyWatching!",
+                      style: TextStyle(color: Colors.grey, fontSize: 10),
+                    ),
+                    Row(
+                      children: [
+                        Text("Finnie Rose",
+                            style: TextStyle(color: Colors.white)),
+                        Icon(
+                          Icons.keyboard_arrow_down,
+                          color: Colors.white,
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  height: 40,
+                  width: 40,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(32)),
+                  child: IconButton(
+                    icon: const Icon(Icons.search),
+                    color: Colors.white,
+                    iconSize: 22,
+                    onPressed: () {},
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Container(
+                  height: 40,
+                  width: 40,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(32)),
+                  child: IconButton(
+                    icon: const Icon(Icons.notifications_none),
+                    color: Colors.white,
+                    iconSize: 22,
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -51,19 +126,62 @@ class AlbumPlayer extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return Container(
+    return SizedBox(
       width: double.infinity,
       height: height,
-      color: Colors.black,
       child: Stack(
         children: [
-          Container(
+          SizedBox(
             width: size.width,
             height: 60,
-            color: Colors.red,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  width: 36,
+                  height: 24,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.red),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: const Text("All", style: TextStyle(color: Colors.red)),
+                ),
+                Container(
+                  width: 42,
+                  height: 24,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: const Text("Series",
+                      style: TextStyle(color: Colors.grey)),
+                ),
+                Container(
+                  width: 42,
+                  height: 24,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child:
+                      const Text("Movie", style: TextStyle(color: Colors.grey)),
+                ),
+                Container(
+                  width: 56,
+                  height: 24,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: const Text("TVShow",
+                      style: TextStyle(color: Colors.grey)),
+                ),
+              ],
+            ),
           ),
           Positioned(
-            top: 60,
+            top: 40,
             child: SizedBox(
               width: size.width,
               height: height - 90,
@@ -71,7 +189,7 @@ class AlbumPlayer extends StatelessWidget {
             ),
           ),
           Positioned(
-            bottom: 10,
+            bottom: 20,
             left: size.width * 0.25,
             right: size.width * 0.25,
             child: SizedBox(
@@ -123,8 +241,10 @@ class AlbumPlayer extends StatelessWidget {
               child: Container(
                 width: 200,
                 decoration: BoxDecoration(
-                  color: Colors.white70,
                   borderRadius: BorderRadius.circular(24),
+                  image: const DecorationImage(
+                      image: AssetImage("assets/images/movie2.jpg"),
+                      fit: BoxFit.cover),
                 ),
               ),
             ),
@@ -136,12 +256,13 @@ class AlbumPlayer extends StatelessWidget {
             child: Transform.rotate(
               angle: 0.20,
               child: Container(
-                width: 200,
-                decoration: BoxDecoration(
-                  color: Colors.purple,
-                  borderRadius: BorderRadius.circular(24),
-                ),
-              ),
+                  width: 200,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(24),
+                    image: const DecorationImage(
+                        image: AssetImage("assets/images/movie3.jpg"),
+                        fit: BoxFit.cover),
+                  )),
             ),
           ),
           Positioned(
@@ -153,6 +274,63 @@ class AlbumPlayer extends StatelessWidget {
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(24),
+                image: const DecorationImage(
+                    image: AssetImage("assets/images/movie1.jpg"),
+                    fit: BoxFit.cover),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  left: 16,
+                  right: 16,
+                  bottom: 16,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          "Interstellar 5555",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Text(
+                          "Movie - 2003",
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      height: 40,
+                      width: 40,
+                      decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(32)),
+                      child: IconButton(
+                        icon: const Icon(Icons.play_arrow_rounded),
+                        color: Colors.white,
+                        iconSize: 24,
+                        onPressed: () {},
+                      ),
+                    ),
+                    Container(
+                      height: 40,
+                      width: 40,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          color: Colors.black,
+                          borderRadius: BorderRadius.circular(32)),
+                      child: IconButton(
+                        icon: const Icon(Icons.add_rounded),
+                        color: Colors.white,
+                        iconSize: 22,
+                        onPressed: () {},
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
@@ -168,10 +346,26 @@ class ContinueWatching extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return SizedBox(
       height: height,
-      child: const Center(child: Text("Continue Watching")),
+      child: const Padding(
+        padding: EdgeInsets.only(
+          left: 16,
+          right: 16,
+        ),
+        child: Row(children: [
+          Icon(
+            Icons.fast_forward_rounded,
+            color: Colors.red,
+            size: 24,
+          ),
+          SizedBox(width: 8),
+          Text(
+            "Continue Watching",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          )
+        ]),
+      ),
     );
   }
 }
